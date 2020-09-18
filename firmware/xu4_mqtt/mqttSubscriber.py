148bd6809dc4c6dfff5ebf05d41708423cf45e33
Teamlary <lhw150030@utdallas.edu>
2020-09-18 15:34:20 -0500

    daily Updates

--- firmware/xu4_mqtt/mqttSubscriber.py
+++ firmware/xu4_mqtt/mqttSubscriber.py
@@ -67,6 +67,6 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.username_pw_set("mintstest", "eryeNYj9Aj")
-client.connect("mqtt.circ.utdallas.edu", 1883, 60)
+client.username_pw_set(mqttUN,mqttPW)
+client.connect(broker, port, 60)
 client.loop_forever()
