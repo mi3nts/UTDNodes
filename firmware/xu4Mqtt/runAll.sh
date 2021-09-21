f1e519fb7b287c00165bdb0c73aa95fe69053074
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2021-09-21 16:27:35 -0500

    Update runAll.sh

--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 #
 sleep 60
-python3 nanoReader0.py &
+python3 nanoReader.py 0 &
 sleep 5
-python3 ips7100ReaderV1.py &
+python3 nanoReader.py 1 &
 sleep 5
-python3 nanoReader1.py &
-sleep 5
-python3 nanoReader2.py &
+python3 nanoReader.py 2 &
 sleep 5
 python3 GPSReader.py &
 sleep 5
 python3 ipReader.py
-
-# python3 centralNodeReaderNano.py &
-# sleep 10
-# cd odroidShow2 && python3 mintsShow2.py
