4aedfbb2fc507e5a2a6cd91f245072c2a36c245c
lakithaomal <lhw150030@utdallas.edu>
2022-01-22 17:01:17 +0000

    Added Stop all

--- firmware/xu4Mqtt/stopAll.sh
+++ firmware/xu4Mqtt/stopAll.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
-
-sleep 60
-kill $(pgrep -f 'python3 nanoReader.py 0')
 sleep 5
-python3 nanoReader.py 0 &
+
+kill $(pgrep -f 'python3 nanoReader.py 0')
 sleep 5
 
 kill $(pgrep -f 'python3 nanoReader.py 1')
 sleep 5
-python3 nanoReader.py 1 &
-sleep 5
 
 kill $(pgrep -f 'python3 nanoReader.py 2')
-sleep 5
-python3 nanoReader.py 2 &
 sleep 5
 
 kill $(pgrep -f 'python3 GPSReader.py')
 sleep 5
-python3 GPSReader.py &
-sleep 5
 
 kill $(pgrep -f 'ips7100ReaderV1.py')
 sleep 5
-python3 ips7100ReaderV1.py &
-sleep 5
 
-python3 ipReader.py
 
 
